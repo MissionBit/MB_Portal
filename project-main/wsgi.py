e75510c5fb621686353ed2cbcd1e19dc50d7db32
Tyler Iams <taiams@dons.usfca.edu>
2019-05-20 16:48:01 -0700

    fixed change dir name issues

--- project-main/wsgi.py
+++ project-main/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'missionbit_portal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project-main.settings')
 
 application = get_wsgi_application()
