c20caca8757b69fcbaaaff698f31ce500a283fcd
Tyler Iams <taiams@dons.usfca.edu>
2019-05-20 13:03:16 -0700

    working on toy

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+posts = [
+		{
+			'author' : 'Tyler',
+			'title' : 'First Post',
+			'content' : 'This is my first post',
+			'date_posted' : '5/20/2019'
+		},
+		{
+			'author' : 'Sophia',
+			'title' : 'My Post',
+			'content' : 'This is my personal post',
+			'date_posted' : '5/21/2019'
+		}
+]
+
+def home(request):
+	context = {
+				'posts' : posts
+	}
+	return render(request, 'blog/home.html', context)
+
+
+def about(request):
+	return render(request, 'blog/about.html', { 'title' : 'Yellow About'})
