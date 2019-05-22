dc60da46bf77da1779f218d6622fbcdf2a7724d6
Tyler Iams <taiams@dons.usfca.edu>
2019-05-22 14:11:09 -0400

    gitignore

--- project-main/urls.py
+++ project-main/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from Users import views as Users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', Users_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
     path('', include('blog.urls'))
 ]
