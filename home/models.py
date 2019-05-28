722be67b15bc32b588beb3aaac61ec91fe987e0b
Tyler Iams <taiams@dons.usfca.edu>
2019-05-28 13:11:45 -0400

    fixed role in User model

--- home/models.py
+++ home/models.py
@@ -3,11 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
+	STAFF = 'STA'
+	TEACHER = 'TEA'
+	STUDENT = 'STU'
+	VOLUNTEER = 'VOL'
+	ROLE_CHOICES = [
+		(STAFF, 'Staff'),
+		(TEACHER, 'Teacher'),
+		(STUDENT, 'Student'),
+		(VOLUNTEER, 'Volunteer')
+	]
 	username = models.CharField(max_length=100, default='none')
 	email = models.CharField(max_length=100)
 	first_name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
-	role = models.CharField(max_length=15)
-
-
-
+	role = models.CharField(max_length=3, choices=ROLE_CHOICES, default=STUDENT)
