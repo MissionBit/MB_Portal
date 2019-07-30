c76018eb24266e039e1efc2521466d928b69dda0
Tyler Iams <tyleriams@Tylers-MacBook-Pro.local>
2019-07-30 16:14:09 -0700

    groups created

---
+++ home/migrations/0002_add_groups.py
@@ -0,0 +1,24 @@
+from django.db import models, migrations
+from home.models.models import Group
+from django.conf import settings
+
+
+def add_groups():
+    Group.objects.get_or_create(name="Staff"),
+    Group.objects.get_or_create(name="Student"),
+    Group.objects.get_or_create(name="Teacher"),
+    Group.objects.get_or_create(name="Volunteer"),
+    Group.objects.get_or_create(name="Donor"),
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("auth", "0011_update_proxy_permissions"),
+        ('home', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_groups),
+    ]
