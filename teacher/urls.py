537b97cb371e45f3f36fb69c862a76b3aa49b9ae
Tyler Iams <tyleriams@Tylers-MacBook-Pro.local>
2019-08-03 14:14:10 -0700

    teacher can view curriculum/add resources/take attendance

--- teacher/urls.py
+++ teacher/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [path("", views.teacher, name="teacher"),
-               path("download_form/", views.download_form, name="download_form")]
+               path("download_form/", views.download_form, name="download_form"),
+               path("my_class/", views.my_class, name="my_class"),
+               path("session_view/", views.session_view, name="session_view")]
--- teacher/views.py
+++ teacher/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from home.decorators import group_required
-from home.models.models import Announcement, Form, Notification
+from home.models.models import Announcement, Form, Notification, Classroom, Session, Resource
+from home.forms import AddResourceForm
+from attendance.views import get_date_from_template_returned_string
 from staff.staff_views_helper import mark_announcement_dismissed, remove_dismissed_announcements, \
     remove_submitted_forms, mark_notification_acknowledged
 import os
 from django.http import HttpResponse, Http404
+from django.contrib import messages
 
 
 @group_required("teacher")
@@ -36,3 +39,41 @@ def download_form(request):
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
             return response
     raise Http404
+
+
+@group_required("teacher")
+def my_class(request):
+    try:
+        classroom = Classroom.objects.get(teacher_id=request.user.id)
+    except Exception:
+        classroom = Classroom.objects.get(teacher_assistant_id=request.user.id)
+    sessions = Session.objects.filter(classroom_id=classroom.id).order_by("date")
+    return render(request, "my_class.html", {"sessions": sessions,
+                                             "classroom": classroom})
+
+
+@group_required("teacher")
+def session_view(request):
+    if request.method == "POST":
+        form = AddResourceForm(request.POST, request.FILES)
+        if form.is_valid():
+            resource = Resource(
+                title=form.cleaned_data.get("title"),
+                description=form.cleaned_data.get("description"),
+                classroom_id=request.POST.get("classroom"),
+                session_id=request.POST.get("session")
+            )
+            if request.POST.get("link"):
+                resource.link = form.cleaned_data.get("link")
+            if request.FILES.get("file"):
+                resource.file = form.cleaned_data.get("file")
+            resource.save()
+            messages.add_message(request, messages.SUCCESS, "Resource Added To Session")
+            return redirect("teacher")
+    session = Session.objects.get(classroom_id=request.GET.get("classroom"),
+                                  date=get_date_from_template_returned_string(request.GET.get("session_date")))
+    resources = Resource.objects.filter(session_id=session.id)
+    form = AddResourceForm()
+    return render(request, "session_view.html", {"session": session,
+                                                 "resources": resources,
+                                                 "form": form})
