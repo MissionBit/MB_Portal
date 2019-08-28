6f0047acbd46ca85a1b4b7280ce2b416ea713233
Tyler Iams <tyleriams@Tylers-MacBook-Pro.local>
2019-08-27 18:18:51 -0700

    celery attempt

--- missionbit/settings.py
+++ missionbit/settings.py
@@ -240,3 +240,18 @@ LOGGING = {
     }
 }
 """
+
+from celery.schedules import crontab
+
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_BEAT_SCHEDULE = {
+    'hello': {
+        'task': 'missionbit.tasks.hello',
+        'schedule': crontab()  # execute every minute
+    }
+}
